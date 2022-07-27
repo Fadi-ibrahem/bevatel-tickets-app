14c738db76ff44dbacd108bb39430122d7446be4
Fadi-ibrahem <fadyibrahem93@gmail.com>
2022-07-27 10:29:21 +0200

    first commit

--- routes/web.php
+++ routes/web.php
@@ -19,5 +19,3 @@ Route::resource('/replies', ReplyController::class);
 
 // A specific Ticket Replies Route
 Route::get('/ticket/{ticket}/replies', [TicketController::class, 'replies'])->name('ticket.replies');
-
-Route::get();
